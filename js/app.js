1a888bd8749f9fd700c6d3832f13ace4c6aec1c7
Natalie Cyreus <nataliecyreus@gmail.com>
2017-12-11 13:27:39 -0500

    separate Cat and the view model

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount  = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   this.imgAttribution = ko.observable('https://');
   this.level = ko.observable('newborn');
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-    this.incrementLevel();
-  };
+  this.nicknames = ko.observableArray(['Kittie', 'hello Kitty']);
 
   this.incrementLevel = function() {
-    if (this.clickCount() >= 20) {
-      this.level('teen')
-    } else if (this.clickCount() >= 10){
+    if (this.clickCount() < 10){
+      this.level('newborn')
+    }else if (this.clickCount() < 20) {
       this.level('infant')
+    } else if (this.clickCount() < 30){
+      this.level('teen')
+    } else if (this.clickCount() < 40){
+      this.level('adult')
+    } else {
+      this.level('ninja')
     }
-  }
+  };
+};
 
-  this.nicknames = ko.observableArray([
-    {nickname:'Kittie'},
-    {nickname: 'hello Kitty'}
-  ]);
-
+var ViewModel = function() {
+  this.currentCat = ko.observable( new Cat() );
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel())
