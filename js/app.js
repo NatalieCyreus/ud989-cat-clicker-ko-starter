06dc5748e1aca594277161bd78306d12a9d58661
Natalie Cyreus <nataliecyreus@gmail.com>
2017-12-11 11:37:42 -0500

    first cat, added level

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ViewModel = function() {
+  this.clickCount  = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+  this.imgAttribution = ko.observable('https://');
+  this.level = ko.observable('newborn');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+    this.incrementLevel();
+  };
+
+  this.incrementLevel = function() {
+    if (this.clickCount() >= 20) {
+      this.level('teen')
+    } else if (this.clickCount() >= 10){
+      this.level('infant')
+    }
+  }
+
+}
+
+ko.applyBindings(new ViewModel())
